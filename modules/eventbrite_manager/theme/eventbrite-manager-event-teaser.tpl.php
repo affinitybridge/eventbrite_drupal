6579cbaba026cf6923822eb699cc85b368bfd635
dtarc <dave@affinitybridge.com>
2010-11-12 03:10:32 -0800

    Adding teaser template

---
+++ modules/eventbrite_manager/theme/eventbrite-manager-event-teaser.tpl.php
@@ -0,0 +1,33 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Template to display date navigation links.
+ *
+ * $event
+ *   The event object which contains all event fields.
+ * 
+ */
+?>
+<hr><hr>
+<div class="eventbrite_manager-event">
+  <div class="eventbrite_manager-event-title">
+    <h3>
+      <?php print($title);?>
+    </h3>
+  </div>
+  
+  <div class="eventbrite_manager-event-date">
+      <?php print $date_range;?>
+  </div>
+  
+  <div class="eventbrite_manager-event-description">
+      <?php print($description);?>
+  </div>
+
+  <div class="eventbrite_manager-event-attend">
+      <?php print($attend_link);?>
+  </div>
+</div>
+  
