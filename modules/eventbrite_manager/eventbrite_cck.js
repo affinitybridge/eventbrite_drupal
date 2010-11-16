dbb996326fd671262bedaf3bb2d1531ec93043c0
dtarc <dave@affinitybridge.com>
2010-11-16 13:48:59 -0800

    Adding js for toggle of load/create

---
+++ modules/eventbrite_manager/eventbrite_cck.js
@@ -0,0 +1,15 @@
+Drupal.behaviors.eventBrite = function (context) {
+  $('fieldset.eventbrite_cck-new-load').hide();
+  $('fieldset.eventbrite_cck-new-create').hide();
+
+  $('input.eventbrite_cck-new-toggle').click(function() {
+    if ($(this).attr('value') == 'load' ) {
+      $('fieldset.eventbrite_cck-new-load').show();
+      $('fieldset.eventbrite_cck-new-create').hide();
+    }
+    else if ($(this).attr('value') == 'create' ) {
+      $('fieldset.eventbrite_cck-new-load').hide();
+      $('fieldset.eventbrite_cck-new-create').show();
+    }
+  });
+};
