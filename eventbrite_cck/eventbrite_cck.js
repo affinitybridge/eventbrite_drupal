d5a02e0a8218cfc523ff75f96613abc166aa54da
dtarc <dave@affinitybridge.com>
2010-11-22 16:11:58 -0800

    Fixed ticket handling, cleaned up module

--- eventbrite_cck/eventbrite_cck.js
+++ eventbrite_cck/eventbrite_cck.js
@@ -1,4 +1,5 @@
 Drupal.behaviors.eventBrite = function (context) {
+  // Toggle between load event and create event
   $('input.eventbrite_cck-new-toggle').click(function() {
     if ($(this).attr('value') == 'load' ) {
       $('fieldset.eventbrite_cck-new-load').show();
@@ -9,4 +10,26 @@ Drupal.behaviors.eventBrite = function (context) {
       $('fieldset.eventbrite_cck-new-create').show();
     }
   });
+  
+  // Set ticket input fields to disabled (readonly does not work for some field elements)
+  //   If ticket update does not get checked these fields will get re-enabled on node submit
+  disableTicketFields(true);
+  $('input.eventbrite_cck-ticket-toggle').attr('disabled', false);
+  
+  // On submit, re-enable all disabled fields so that values get submitted
+  $('#edit-submit').click(function() {
+    disableTicketFields(false);
+  });
+  
+  // Trying to toggle whether ticket fields are editable
+  $('input.eventbrite_cck-ticket-toggle').click(function() {
+    disableTicketFields(false);
+    $('input.eventbrite_cck-ticket-toggle').attr('disabled', true);
+  });
+
+  function disableTicketFields($disabled) {
+    $('input.eventbrite_cck-ticket-input').attr('disabled', $disabled);
+    $('textarea.eventbrite_cck-ticket-input').attr('disabled', $disabled);
+    $('input.eventbrite_cck-ticket-toggle').attr('disabled', $disabled);
+  }
 };
